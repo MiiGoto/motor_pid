3fe9ace21fca14ad2755fdc128a86db4831fed0e
mii <109723126+MiiGoto@users.noreply.github.com>
2022-12-21 18:25:24 +0900

    Update PID.h

--- PID.h
+++ PID.h
@@ -1,26 +1,20 @@
-#ifndef _PID_H
-#define _PID_H
+#ifndef pid_h
+#define pid_h
 #include <Arduino.h>
 
-class MoveB {
+class Pid{
   public:
-    int pitchCommand;
-    int pitchValue;
-    int nowspd;
-
+    Pid();
+    void init(float p_gain,float i_gain,float d_gain);
+    void now_value(int measure);
+    int pid_out(int target);
+    int debug();
   private:
-    const float pgain[3];
-    static float pIe;
-    static float prepPe;
-    static float pPe;
+    float gain[3];
+    int in, pre, dif;
+    int last_measure;
+    int pre_measure[10];
+    int measure_tim;
 };
-
-int PIDh0(int pitchCommand, int pitchValue, int nowspd);
-
-int PIDh1(int pitchCommand, int pitchValue, int nowspd);
-
-int PIDh2(int pitchCommand, int pitchValue, int nowspd);
-
-int PIDh3(int pitchCommand, int pitchValue, int nowspd);
 
 #endif
